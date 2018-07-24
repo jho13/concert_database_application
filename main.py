86f87ef5450b0bbcee62e1e3d971359168763d02
Jonghoon <tnekaos1024@gmail.com>
2018-07-24 11:58:06 +0900

    Implemented all functions, need to fix issues commented at top

--- main.py
+++ main.py
@@ -5,33 +5,374 @@ Created on Thu Jul 19 17:34:45 2018
 @author: 오종훈
 """
 
+#fetch one 
+#checks
+#newlines
+
+import pymysql.cursors
+
 def print_commands():
-    print('''
-          ============================================================ 
-          1. print all buildings 
-          2. print all performances 
-          3. print all audiences 
-          4. insert a new building 
-          5. remove a building 
-          6. insert a new performance 
-          7. remove a performance 
-          8. insert a new audience 
-          9. remove an audience 
-          10. assign a performance to a building 
-          11. book a performance 
-          12. print all performances assigned to a building 
-          13. print all audiences who booked for a performance 
-          14. print ticket booking status of a performance 
-          15. exit 
-          ============================================================
-          ''')
+    print(
+'''============================================================ 
+1. print all buildings 
+2. print all performances 
+3. print all audiences 
+4. insert a new building 
+5. remove a building 
+6. insert a new performance 
+7. remove a performance 
+8. insert a new audience 
+9. remove an audience 
+10. assign a performance to a building 
+11. book a performance 
+12. print all performances assigned to a building 
+13. print all audiences who booked for a performance 
+14. print ticket booking status of a performance 
+15. exit 
+============================================================''')
+
+def print_building_header():
+    print(
+'''-------------------------------------------------------------------------------- 
+id    name                      location      capacity      assigned
+--------------------------------------------------------------------------------''')
     
+def print_building_data(rows):
+    for row in rows:
+        values = row.values()
+        print('{:<6}{:<26}{:<14}{:<14}{:<14}'.format(*values))
+    if (len(rows) != 0):
+        print('----------------------------------------'
+              '----------------------------------------')
+        
+def print_performance_header(print_bid = True):
+    if (print_bid):
+        print(
+'''-------------------------------------------------------------------------------- 
+id    name                      type          price         booked        builidng_id
+--------------------------------------------------------------------------------''')
+    else:
+        print(
+'''-------------------------------------------------------------------------------- 
+id    name                      type          price         booked
+--------------------------------------------------------------------------------''')
+    
+def print_performance_data(rows, print_bid = True):
+    for row in rows:
+        values = row.values()
+        print('{:<6}{:<26}{:<14}{:<14}{:<14}'.format(*list(values)[:-1]), end = '') 
+        if (print_bid):
+            if (row['building_id'] != None):
+                print('{:<6}'.format(row['building_id']))     
+    if (len(rows) != 0):
+        print('----------------------------------------'
+              '----------------------------------------')
+        
+def print_audience_header():
+    print(
+'''-------------------------------------------------------------------------------- 
+id    name                              gender        age
+--------------------------------------------------------------------------------''')
+    
+def print_audience_data(rows):
+    for row in rows:
+        values = row.values()
+        print('{:<6}{:<36}{:<14}{:<14}'.format(*values))
+    if (len(rows) != 0):
+        print('----------------------------------------'
+              '----------------------------------------')
+        
+def print_seat_header():
+    print(
+'''-------------------------------------------------------------------------------- 
+seat_number                     audience_id
+--------------------------------------------------------------------------------''')
+        
+def print_seat_data(rows):
+    for row in rows:
+        print('{:<36}'.format(row['seat_number']), end = '')
+        if (row['audience_id'] != None):
+            print('{:<6}'.format(row['audience_id']))
+        else:
+            print()
+            
+    if (len(rows) != 0):
+        print('----------------------------------------'
+              '----------------------------------------')
+
+
+
+def connect_to_database():
+    return pymysql.connect(
+        host = 'astronaut.snu.ac.kr',
+        user = 'BDE-2018-14',
+        password = '0d00f6382a49',
+        db = 'BDE-2018-14',
+        charset = 'utf8',
+        autocommit = True,
+        cursorclass = pymysql.cursors.DictCursor)
+    
+def print_buildings(cursor):
+    print_building_header()
+    cursor.execute(
+            'SELECT * '
+            'FROM building '
+            'ORDER BY id '
+    )
+    print_building_data(cursor.fetchall())
+
+def print_performances(cursor):
+    print_performance_header()
+    cursor.execute(
+            'SELECT * ' 
+            'FROM performance '
+            'ORDER BY id '
+    )
+    print_performance_data(cursor.fetchall())
+
+def print_audiences(cursor):
+    print_audience_header()
+    cursor.execute(
+            'SELECT * '
+            'FROM audience '
+            'ORDER BY id '
+    )
+    print_audience_data(cursor.fetchall())
+
+def insert_building(cursor):
+    name = input('Building name: ')
+    location = input('Building location: ')
+    capacity = int(input('Building capacity: '))
+    if (capacity <= 0):
+        print('Capacity should be greater than 0!')
+        return
+    
+    cursor.execute(
+            'INSERT INTO building(name, location, capacity, assigned) '
+            'VALUES(%s, %s, %s, FALSE)',
+            (name, location, capacity)
+    )
+    print('A building is successfully inserted')
+
+def delete_building(cursor):
+    b_id = int(input('Building ID: '))
+    num_rows = cursor.execute(
+            'DELETE FROM building '
+            'WHERE id = %s ',
+            b_id
+    )
+    if (num_rows == 0):
+        print('Building with ID', b_id, 'does not exist!')
+    else:
+        print('Succesfully deleted')
+
+def insert_performance(cursor):
+    name = input('Performance name: ')
+    tpe = input('Performance type: ')
+    price = int(input('Performance price: '))
+    if (price <= 0):
+        print('Price should be greater than 0!')
+        return
+    
+    cursor.execute(
+            'INSERT INTO performance(name, type, price) '
+            'VALUES(%s, %s, %s)',
+            (name, tpe, price)
+    )
+    print('A performance is successfully inserted')
+
+def delete_performance(cursor):
+    p_id = int(input('Performance ID: '))
+    num_rows = cursor.execute(
+            'DELETE FROM performance '
+            'WHERE id = %s',
+            p_id
+    )
+    if (num_rows == 0):
+        print('Performance with ID', p_id, 'does not exist!')
+    else:
+        print('Succesfully deleted')
+
+def insert_audience(cursor):
+    name = input('Audience name: ')
+    gender = input('Audience gender: ')
+    age = int(input('Audience age: '))
+    if (age <= 0):
+        print('Age should be greater than 0!')
+        return
+    
+    cursor.execute(
+            'INSERT INTO audience(name, gender, age) '
+            'VALUES(%s, %s, %s)',
+            (name, gender, age)
+    )
+    print('An audience is successfully inserted')
+
+def delete_audience(cursor):
+    a_id = input('Audience ID: ')
+    num_rows = cursor.execute(
+            'DELETE FROM audience '
+            'WHERE id = %s',
+            a_id
+    )
+    if (num_rows == 0):
+        print('Audience with ID', a_id, 'does not exist!')
+    else:
+        print('Succesfully deleted')
+
+def assign_builing_to_performance(cursor):
+    b_id = int(input('Building ID: '))
+    num_rows = cursor.execute(
+            'SELECT * '
+            'FROM building '
+            'WHERE id = %s ',
+            b_id
+    )
+    if (num_rows == 0):
+        print('Building with ID', b_id, 'does not exist!')
+        return
+    capacity = cursor.fetchone()['capacity']
+    
+    p_id = int(input('Performance ID: '))
+    num_rows = cursor.execute(
+            'SELECT * '
+            'FROM performance '
+            'WHERE id = %s ',
+            p_id
+    )
+    if (num_rows == 0):
+        print('Performance with ID', p_id, 'does not exist!')
+        return
+    
+    row = cursor.fetchone()
+    if (row['building_id'] != None):
+        print('A building is already assigned to this performance!')
+     
+    # assign building to performance
+    cursor.execute(
+            'UPDATE performance '
+            'SET building_id = %s '
+            'WHERE id = %s ',
+            (b_id, p_id)
+    )
+    
+    # create seats
+    cursor.executemany(
+            'INSERT INTO seat(seat_number, performance_id) ' 
+            'VALUES(%s, %s) ',
+            [[i + 1, p_id] for i in range(capacity)]
+    )
+    
+    #for i in range(capacity):
+    #    cursor.execute(
+    #            'INSERT INTO seat(seat_number, performance_id) ' 
+    #            'VALUES(%s, %s) ',
+    #            (i + 1, p_id)
+    #    )
+    print('Successfully assigned a performance')
+
+def reserve_seats_for_performance(cursor):
+    p_id = int(input('Performance ID: '))
+    a_id = int(input('Audience ID: '))
+    seats = tuple(int(x) for x in input('Seat number: ').split(','))
+    
+    cursor.execute(
+            'UPDATE seat '
+            'SET audience_id = %s '
+            'WHERE performance_id = %s AND seat_number IN (%s) ',
+            (a_id, p_id, seats)
+    )
+    
+def print_buildings_performances(cursor):
+    b_id = int(input('Building ID: '))
+    num_rows = cursor.execute(
+            'SELECT * '
+            'FROM building '
+            'WHERE id = %s '
+            'ORDER BY id ',
+            b_id
+    )
+    if (num_rows == 0):
+        print('Building with ID', b_id, 'does not exist!')
+        return
+    
+    print_performance_header(False)
+    cursor.execute(
+            'SELECT * '
+            'FROM performance '
+            'WHERE building_id = %s',
+            b_id
+    )
+    print_performance_data(cursor.fetchall(), False)
+
+def print_performances_audiences(cursor):
+    p_id = int(input('Performance ID: '))
+    num_rows = cursor.execute(
+            'SELECT * '
+            'FROM performance '
+            'WHERE id = %s ',
+            p_id
+    )
+    if (num_rows == 0):
+        print('Performance with ID', p_id, 'does not exist!')
+        return
+    
+    print_audience_header()
+    cursor.execute(
+            'SELECT DISTINCT a.* '
+            'FROM audience a JOIN seat s ON(a.id = s.audience_id) '
+            'WHERE s.performance_id = %s '
+            'ORDER BY a.id ',
+            p_id
+    )
+    print_audience_data(cursor.fetchall())
+
+def print_performances_seats(cursor):
+    p_id = int(input('Performance ID: '))
+    num_rows = cursor.execute(
+            'SELECT * '
+            'FROM performance '
+            'WHERE id = %s ',
+            p_id
+    )
+    if (num_rows == 0):
+        print('Performance with ID', p_id, 'does not exist!')
+        return
+    
+    if (cursor.fetchone()['building_id'] == None):
+        print('No building assigned to performance!')
+        return
+    
+    print_seat_header()
+    cursor.execute(
+            'SELECT s.seat_number, s.audience_id '
+            'FROM seat s '
+            'WHERE s.performance_id = %s ',
+            p_id
+    )
+    print_seat_data(cursor.fetchall())
+    
+action_list = [print_buildings, print_performances, print_audiences, insert_building,
+               delete_building, insert_performance, delete_performance, insert_audience,
+               delete_audience, assign_builing_to_performance, reserve_seats_for_performance,
+               print_buildings_performances, print_performances_audiences, print_performances_seats]
+
+
 
 if (__name__ == '__main__'):
     print_commands()
+    connection = connect_to_database()
+    cursor = connection.cursor()
     
-    action = -1
+    action = None
     while (action != 15):
-        
         action = int(input('Select your action: '))
+        if (action >= 1 and action <= 14):
+            action_list[action - 1](cursor)
+        elif (action == 15):
+            print('Bye!')
+        else:
+            print('Invalid action! Enter a number between 1 and 15.')
         
+    cursor.close()
+    connection.close()
